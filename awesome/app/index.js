b04e2f81352873e9259de795a13a66da821bde82
Mutuku1998 <martinmutuku1998@gmail.com>
2024-07-04 18:36:12 +0300

    start of journey

---
+++ awesome/app/index.js
@@ -0,0 +1,13 @@
+import { View,Text } from "react-native-web";
+
+const Home = () =>{
+
+    return(
+
+        <View>
+            <Text>
+            HOME
+            </Text>
+        </View>
+    )
+}
